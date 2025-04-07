262d22d9be4ed1c261f70ef008ddd98de3ff1a7c
akirapf3 <38709436+akirap3@users.noreply.github.com>
2025-04-07 04:27:33 +0000

    complete ch3_FizzBuzz

--- src/03/Challenge/ch3_FizzBuzz.cpp
+++ src/03/Challenge/ch3_FizzBuzz.cpp
@@ -11,9 +11,23 @@
 // Summary: This application runs on the main function.
 int main(){
     int n; // How many numbers to include in the sequence.
+    bool b, f;
     
     std::cout << "Enter a positive integer: " << std::flush;
     std::cin >> n;
+
+    for(int i = 1; i <=n; i++) {
+        f = (i % 3) == 0;
+        b = (i % 5) == 0;
+
+        if(!f && !b) {
+            std::cout << i;
+        } else {
+            if(f) std::cout << "Fizz";
+            if(b) std::cout << "Buzz";
+        }
+        std::cout << std::endl;
+    }
 
     // Write your code here
     
